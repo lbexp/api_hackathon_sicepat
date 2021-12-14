6e399115dd7fcfb0e4dbf1bbd826673d48855984
Fiqar Aprialim <fiqarapr@gmail.com>
2021-12-14 22:51:27 +0800

    feat: dashboard index & detail

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard/index');
+});
+
+Route::get('/{id}', function () {
+    return view('dashboard/detail');
 });
