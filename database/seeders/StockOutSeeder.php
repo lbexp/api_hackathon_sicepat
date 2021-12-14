b01e050447a7c4158db3235a9e54ce7431d2aa78
Fiqar Aprialim <fiqarapr@gmail.com>
2021-12-14 20:04:08 +0800

    fix: items var

--- database/seeders/StockOutSeeder.php
+++ database/seeders/StockOutSeeder.php
@@ -19,23 +19,41 @@ class StockOutSeeder extends Seeder
         for ($i = 1825; $i >= 0; $i--) {
             $date = Carbon::now()->subDays($i)->format('Y-m-d H:i:s');
             
+            // $items = [
+            //     'Apple iPhone 13',
+            //     'Apple iPhone 13 Pro',
+            //     'Samsung Galaxy S21',
+            //     'Samsung Galaxy S21 Ultra',
+            //     'Samsung Galaxy Z Fold3',
+            //     'Samsung Galaxy Z Flip3',
+            //     'Xiaomi Mix 4',
+            //     'Xiaomi Mi 11',
+            //     'Xiaomi Mi 11 Ultra',
+            //     'Xiaomi Mi 11 Pro',
+            //     'Xiaomi Mi Mix Fold',
+            //     'Xiaomi 11T Pro',
+            //     'Google Pixel 6 Pro',
+            //     'Google Pixel 6',
+            //     'Vivo X70 Pro',
+            //     'Huawei Mate X2'
+            // ];
             $items = [
-                'Apple iPhone 13',
-                'Apple iPhone 13 Pro',
-                'Samsung Galaxy S21',
-                'Samsung Galaxy S21 Ultra',
-                'Samsung Galaxy Z Fold3',
-                'Samsung Galaxy Z Flip3',
-                'Xiaomi Mix 4',
-                'Xiaomi Mi 11',
-                'Xiaomi Mi 11 Ultra',
-                'Xiaomi Mi 11 Pro',
-                'Xiaomi Mi Mix Fold',
-                'Xiaomi 11T Pro',
-                'Google Pixel 6 Pro',
-                'Google Pixel 6',
-                'Vivo X70 Pro',
-                'Huawei Mate X2'
+                'item_1',
+                'item_2',
+                'item_3',
+                'item_4',
+                'item_5',
+                'item_6',
+                'item_7',
+                'item_8',
+                'item_9',
+                'item_10',
+                'item_11',
+                'item_12',
+                'item_13',
+                'item_14',
+                'item_15',
+                'item_16',
             ];
 
             foreach ($items as $item) {
