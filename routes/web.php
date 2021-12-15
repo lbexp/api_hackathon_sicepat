d71b14ada6682265e193949ea32cd795df134fc4
Fiqar Aprialim <fiqarapr@gmail.com>
2021-12-15 16:00:00 +0800

    fix: route dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard/index');
-});
+// Route::get('/', function () {
+//     return view('dashboard/index');
+// });
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+
 
 Route::get('/{id}', function () {
     return view('dashboard/detail');
